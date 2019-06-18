1b634d59f5d4062ec1f8fe44aa077b37bb6cb4e2
Joel Pickin <joelpickin@outlook.com>
2019-06-18 20:20:51 +0100

    Started Fresh

--- PersonalWebsite/PersonalWebsite/Program.cs
+++ PersonalWebsite/PersonalWebsite/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace PersonalWebsiteJP
+namespace PersonalWebsite
 {
     public class Program
     {
